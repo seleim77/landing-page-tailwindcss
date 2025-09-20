adaeabd31e821dc3eff354d947433024c7edd4fd
seleim77 <abdelrahmanselim12345@gmail.com>
2025-09-20 15:37:03 +0300

    partners check point

--- src/main.js
+++ src/main.js
@@ -3,4 +3,12 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
+const navBtn = document.querySelector("#menu");
+const menuBar = document.querySelector("[role='menubar']");
 
+navBtn.addEventListener("click", () => {
+    const isExpanded = JSON.parse(navBtn.getAttribute('aria-expanded'));
+    navBtn.setAttribute('aria-expanded', !isExpanded);
+    menuBar.classList.toggle("hidden");
+    menuBar.classList.toggle("flex");
+})
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  experimental: {
+    alwaysAddHover: true,
+  },
+  // other config options here
+};
